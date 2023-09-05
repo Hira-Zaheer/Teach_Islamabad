0b9169a782d8c48a8a198a37bb21740154c08745
Hira-Zaheer <hirazaheer842@gmail.com>
2023-09-05 23:16:27 +0500

    reg

--- app/src/main/java/com/example/teachislamabad/reg.kt
+++ app/src/main/java/com/example/teachislamabad/reg.kt
@@ -1,5 +1,6 @@
 package com.example.teachislamabad
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,6 +10,13 @@ class reg : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reg)
 
+        var login = findViewById<Button>(R.id.bt_login)
+        login.setOnClickListener{ val intent =Intent(this,Login::class.java)
+            startActivity(intent) }
+
+        var signup = findViewById<Button>(R.id.bt_signup)
+        signup.setOnClickListener{ val intent = Intent(this,SignUp::class.java)
+        startActivity(intent) }
 
     }
 }
